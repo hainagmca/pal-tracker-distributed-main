58c67bc6f74115b507b8033f9ababd9852242be8
chundn4 <nagendra.chunduru@emc.com>
2019-04-16 16:13:30 +0530

    Solution for Eureka Service Discovery https://courses.education.pivotal.io/c/349802933/cloud-native-developer/spring-cloud-developer/service-discovery/index.html

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker.allocations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
@@ -10,12 +11,12 @@ import org.springframework.web.client.RestOperations;
 import java.util.TimeZone;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"io.pivotal.pal.tracker.allocations", "io.pivotal.pal.tracker.restsupport"})
 public class App {
 
     public static void main(String[] args) {
-        //Adding a comment
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
     }
